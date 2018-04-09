c11c32da39ea5671b84051246648e78386016d4e
houjincheng <houjincheng1992@163.com>
2018-04-09 15:25:48 +0800

    feat: 针对开发环境进行配置修改

--- jihe/settings/dev.py
+++ jihe/settings/dev.py
@@ -2,6 +2,7 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+ALLOWED_HOSTS=['.tiffcode.com']
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '7&r0r@5s^fpt=l%3b9&68d$k)_#cty)9%&f$71qf2skg*sy%gt'
